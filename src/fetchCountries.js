189efb9948cfe22e56e925f7230a5274d90be9ac
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-07-11 16:41:27 +0200

    11

--- src/fetchCountries.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2/name';
-
-export default function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/${searchQuery}`).then(response => response.json());
-}
--- src/get-refs.js
+++
@@ -1,6 +0,0 @@
-export default function getRefs() {
-    return {
-        input: document.querySelector('.js-search-input'),
-  countriesContainer: document.querySelector('.js-countries-container'),
-    }
-}
